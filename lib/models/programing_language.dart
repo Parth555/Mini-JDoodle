70c62917db63ed13ceae3ca45e3edef6c026411b
parth-benzatine <parth@benzatine.com>
2024-09-12 11:25:47 +0530

    init commit

--- lib/models/programing_language.dart
+++ lib/models/programing_language.dart
@@ -611,5 +611,286 @@ List<ProgrammingLanguage> programmingLanguages = [
       Version(version: '20.9.0', index: 5),
       Version(version: '21.2.0', index: 6),
     ],
+  ),ProgrammingLanguage(
+    name: 'Objective-C',
+    languageCode: 'objc',
+    versions: [
+      Version(version: 'GCC 5.3.0', index: 0),
+      Version(version: 'GCC 7.2.0', index: 1),
+      Version(version: 'GCC 8.1.0', index: 2),
+      Version(version: 'GCC 9.1.0', index: 3),
+      Version(version: 'GCC 11.1.0', index: 4),
+      Version(version: 'GCC 13.2.1', index: 5),
+    ],
+  ),ProgrammingLanguage(
+    name: 'Ocaml',
+    languageCode: 'ocaml',
+    versions: [
+      Version(version: '4.03.0', index: 0),
+      Version(version: '4.08.1', index: 1),
+      Version(version: '4.12.0', index: 2),
+      Version(version: '5.0.0', index: 3),
+    ],
+  ),ProgrammingLanguage(
+    name: 'OZ Mozart',
+    languageCode: 'mozart',
+    versions: [
+      Version(version: '2.0.0 (OZ 3)', index: 0),
+    ],
+  ),
+  ProgrammingLanguage(
+    name: 'Pascal',
+    languageCode: 'pascal',
+    versions: [
+      Version(version: 'fpc 3.0.0', index: 0),
+      Version(version: 'fpc-3.0.2', index: 1),
+      Version(version: 'fpc-3.0.4', index: 2),
+      Version(version: 'fpc-3.2.2', index: 3),
+    ],
+  ), ProgrammingLanguage(
+    name: 'Picolisp',
+    languageCode: 'picolisp',
+    versions: [
+      Version(version: '3.1.11.1', index: 0),
+      Version(version: '17.11.14', index: 1),
+      Version(version: '18.5.11', index: 2),
+      Version(version: '18.9.5', index: 3),
+      Version(version: '21.6.30', index: 4),
+      Version(version: '23.9.29', index: 5),
+    ],
+  ), ProgrammingLanguage(
+    name: 'Pike',
+    languageCode: 'pike',
+    versions: [
+      Version(version: 'v8.0', index: 0),
+      Version(version: 'v8.0.702', index: 1),
+    ],
+  ),ProgrammingLanguage(
+    name: 'Prolog',
+    languageCode: 'prolog',
+    versions: [
+      Version(version: 'GNU Prolog 1.4.4', index: 0),
+      Version(version: 'GNU Prolog 1.4.5', index: 1),
+      Version(version: 'GNU Prolog 1.5.0', index: 2),
+    ],
+  ),
+  ProgrammingLanguage(
+    name: 'PHP',
+    languageCode: 'php',
+    versions: [
+      Version(version: '5.6.16', index: 0),
+      Version(version: '7.1.11', index: 1),
+      Version(version: '7.2.5', index: 2),
+      Version(version: '7.3.10', index: 3),
+      Version(version: '8.0.13', index: 4),
+      Version(version: '8.2.12', index: 5),
+    ],
+  ),ProgrammingLanguage(
+    name: 'Perl',
+    languageCode: 'perl',
+    versions: [
+      Version(version: '5.22.0', index: 0),
+      Version(version: '5.26.1', index: 1),
+      Version(version: '5.26.2', index: 2),
+      Version(version: '5.30.0', index: 3),
+      Version(version: '5.34.0', index: 4),
+      Version(version: '5.38.0', index: 5),
+    ],
+  ),ProgrammingLanguage(
+    name: 'Python 2',
+    languageCode: 'python2',
+    versions: [
+      Version(version: '2.7.11', index: 0),
+      Version(version: '2.7.15', index: 1),
+      Version(version: '2.7.16', index: 2),
+      Version(version: '2.7.18', index: 3),
+    ],
+  ),ProgrammingLanguage(
+    name: 'Python 3',
+    languageCode: 'python3',
+    versions: [
+      Version(version: '3.5.1', index: 0),
+      Version(version: '3.6.3', index: 1),
+      Version(version: '3.6.5', index: 2),
+      Version(version: '3.7.4', index: 3),
+      Version(version: '3.9.9', index: 4),
+      Version(version: '3.11.5', index: 5),
+    ],
+  ),ProgrammingLanguage(
+    name: 'R Language',
+    languageCode: 'r',
+    versions: [
+      Version(version: '3.3.1', index: 0),
+      Version(version: '3.4.2', index: 1),
+      Version(version: '3.5.0', index: 2),
+      Version(version: '3.6.1', index: 3),
+      Version(version: '4.1.2', index: 4),
+      Version(version: '4.3.2', index: 5),
+    ],
+  ),ProgrammingLanguage(
+    name: 'Racket',
+    languageCode: 'racket',
+    versions: [
+      Version(version: '6.11', index: 0),
+      Version(version: '6.12', index: 1),
+      Version(version: '7.4', index: 2),
+      Version(version: '8.3', index: 3),
+    ],
+  ),ProgrammingLanguage(
+    name: 'Raku',
+    languageCode: 'raku',
+    versions: [
+      Version(version: '2023.1', index: 0),
+    ],
+  ),ProgrammingLanguage(
+    name: 'Rhino JS',
+    languageCode: 'rhino',
+    versions: [
+      Version(version: '1.7.7.1', index: 0),
+      Version(version: '1.7.7.2', index: 1),
+      Version(version: '1.7.13', index: 2),
+      Version(version: '1.7.14', index: 3),
+    ],
+  ),ProgrammingLanguage(
+    name: 'Ruby',
+    languageCode: 'ruby',
+    versions: [
+      Version(version: '2.2.4', index: 0),
+      Version(version: '2.4.2p198', index: 1),
+      Version(version: '2.5.1p57', index: 2),
+      Version(version: '2.6.5', index: 3),
+      Version(version: '3.0.2', index: 4),
+      Version(version: '3.0.6', index: 5),
+    ],
+  ),ProgrammingLanguage(
+    name: 'Rust',
+    languageCode: 'rust',
+    versions: [
+      Version(version: '1.10.0', index: 0),
+      Version(version: '1.21.0', index: 1),
+      Version(version: '1.25.0', index: 2),
+      Version(version: '1.38.0', index: 3),
+      Version(version: '1.56.1', index: 4),
+      Version(version: '1.73.0', index: 5),
+    ],
+  ),ProgrammingLanguage(
+    name: 'Scala',
+    languageCode: 'scala',
+    versions: [
+      Version(version: '2.12.0', index: 0),
+      Version(version: '2.12.4', index: 1),
+      Version(version: '2.12.5', index: 2),
+      Version(version: '2.13.0', index: 3),
+      Version(version: '2.13.6', index: 4),
+      Version(version: '2.13.12', index: 5),
+    ],
+  ),ProgrammingLanguage(
+    name: 'Scheme',
+    languageCode: 'scheme',
+    versions: [
+      Version(version: 'Gauche 0.9.4', index: 0),
+      Version(version: 'Gauche 0.9.5', index: 1),
+      Version(version: 'Gauche 0.9.8', index: 2),
+      Version(version: 'Gauche 0.9.10', index: 3),
+      Version(version: 'Gauche 0.9.13', index: 4),
+    ],
+  ),ProgrammingLanguage(
+    name: 'SmallTalk',
+    languageCode: 'smalltalk',
+    versions: [
+      Version(version: 'GNU SmallTalk 3.2.92', index: 0),
+    ],
+  ),
+  ProgrammingLanguage(
+    name: 'SpiderMonkey',
+    languageCode: 'spidermonkey',
+    versions: [
+      Version(version: '38', index: 0),
+      Version(version: '45.0.2', index: 1),
+      Version(version: '91', index: 2),
+    ],
+  ),
+  ProgrammingLanguage(
+    name: 'SQL',
+    languageCode: 'sql',
+    versions: [
+      Version(version: 'SQLite 3.9.2', index: 0),
+      Version(version: 'SQLite 3.21.0', index: 1),
+      Version(version: 'SQLite 3.23.1', index: 2),
+      Version(version: 'SQLite 3.29.0', index: 3),
+      Version(version: 'SQLite 3.37.0', index: 4),
+      Version(version: 'SQLite 3.44.0', index: 5),
+    ],
+  ),ProgrammingLanguage(
+    name: 'Swift',
+    languageCode: 'swift',
+    versions: [
+      Version(version: '2.2', index: 0),
+      Version(version: '3.1.1', index: 1),
+      Version(version: '4.1', index: 2),
+      Version(version: '5.1', index: 3),
+      Version(version: '5.5', index: 4),
+      Version(version: '5.9.1', index: 5),
+    ],
+  ),ProgrammingLanguage(
+    name: 'TCL',
+    languageCode: 'tcl',
+    versions: [
+      Version(version: '8.6', index: 0),
+      Version(version: '8.6.7', index: 1),
+      Version(version: '8.6.8', index: 2),
+      Version(version: '8.6.9', index: 3),
+      Version(version: '8.6.12', index: 4),
+      Version(version: '8.6.13', index: 5),
+    ],
+  ),ProgrammingLanguage(
+    name: 'TypeScript',
+    languageCode: 'typescript',
+    versions: [
+      Version(version: '5.2.2', index: 0),
+    ],
+  ),ProgrammingLanguage(
+    name: 'Unlambda',
+    languageCode: 'unlambda',
+    versions: [
+      Version(version: '0.1.3', index: 0),
+      Version(version: '0.1.4.2', index: 1),
+    ],
+  ),
+  ProgrammingLanguage(
+    name: 'VB. Net',
+    languageCode: 'vbn',
+    versions: [
+      Version(version: 'mono 4.0.1', index: 0),
+      Version(version: 'mono 4.6', index: 1),
+      Version(version: 'mono 5.10.1', index: 2),
+      Version(version: 'mono 6.0.0', index: 3),
+      Version(version: 'mono 6.12.0', index: 4),
+      Version(version: 'dotnet: 7.0.13', index: 5),
+    ],
+  ),ProgrammingLanguage(
+    name: 'VERILOG',
+    languageCode: 'verilog',
+    versions: [
+      Version(version: '10.1', index: 0),
+      Version(version: '10.2', index: 1),
+      Version(version: '10.3', index: 2),
+      Version(version: '11', index: 3),
+      Version(version: '12', index: 4),
+    ],
+  ),ProgrammingLanguage(
+    name: 'Whitespace',
+    languageCode: 'whitespace',
+    versions: [
+      Version(version: '0.3', index: 0),
+    ],
+  ),ProgrammingLanguage(
+    name: 'YaBasic',
+    languageCode: 'yabasic',
+    versions: [
+      Version(version: '2.769', index: 0),
+      Version(version: '2.84.11', index: 1),
+      Version(version: '2.90.4', index: 2),
+    ],
   ),
 ];
